c91f90a1976a638a0a7d9d9fac31c596bc8c32c6
lesander <lesander@users.noreply.github.com>
2021-01-30 18:36:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fbgs",
-    version="0.0.1",
+    version="1.0.0",
     description="Python and selenium based (mobile) Facebook groups scraper, independent of obfuscated css selectors.",
     long_description=long_description,
     long_description_content_type="text/markdown",
